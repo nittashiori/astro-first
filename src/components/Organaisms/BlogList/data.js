2fb96e80ff4c64b08b068ce0317392ec31d5c35b
nittashiori <nitta@impv.co.jp>
2022-12-01 15:32:32 +0900

    BlogListコンポーネント追加

---
+++ src/components/Organaisms/BlogList/data.js
@@ -0,0 +1,81 @@
+import ImageBlog01 from '../../../assets/images/thumbnail01.jpg';
+import ImageBlog2x01 from '../../../assets/images/thumbnail01@2x.jpg';
+import ImageWebpBlog01 from '../../../assets/images/thumbnail01.webp';
+import ImageWebp2xBlog01 from '../../../assets/images/thumbnail01@2x.webp';
+import ImageBlog02 from '../../../assets/images/thumbnail02.jpg';
+import ImageBlog2x02 from '../../../assets/images/thumbnail02@2x.jpg';
+import ImageWebpBlog02 from '../../../assets/images/thumbnail02.webp';
+import ImageWebp2xBlog02 from '../../../assets/images/thumbnail02@2x.webp';
+import ImageBlog03 from '../../../assets/images/thumbnail03.jpg';
+import ImageBlog2x03 from '../../../assets/images/thumbnail03@2x.jpg';
+import ImageWebpBlog03 from '../../../assets/images/thumbnail03.webp';
+import ImageWebp2xBlog03 from '../../../assets/images/thumbnail03@2x.webp';
+import ImageBlog04 from '../../../assets/images/thumbnail04.jpg';
+import ImageBlog2x04 from '../../../assets/images/thumbnail04@2x.jpg';
+import ImageWebpBlog04 from '../../../assets/images/thumbnail04.webp';
+import ImageWebp2xBlog04 from '../../../assets/images/thumbnail04@2x.webp';
+import ImageBlog05 from '../../../assets/images/thumbnail05.jpg';
+import ImageBlog2x05 from '../../../assets/images/thumbnail05@2x.jpg';
+import ImageWebpBlog05 from '../../../assets/images/thumbnail05.webp';
+import ImageWebp2xBlog05 from '../../../assets/images/thumbnail05@2x.webp';
+import ImageBlog06 from '../../../assets/images/thumbnail06.jpg';
+import ImageBlog2x06 from '../../../assets/images/thumbnail06@2x.jpg';
+import ImageWebpBlog06 from '../../../assets/images/thumbnail06.webp';
+import ImageWebp2xBlog06 from '../../../assets/images/thumbnail06@2x.webp';
+
+export const lists = [
+  {
+    link: "#",
+    image: ImageBlog01,
+    image2x: ImageBlog2x01,
+    imageWebp: ImageWebpBlog01,
+    imageWebp2x: ImageWebp2xBlog01,
+    title: "コーヒーカー始めました。",
+    data: "2030.08.08"
+  },
+  {
+    link: "#",
+    image: ImageBlog02,
+    image2x: ImageBlog2x02,
+    imageWebp: ImageWebpBlog02,
+    imageWebp2x: ImageWebp2xBlog02,
+    title: "濃厚クリーミーなミルクをどうぞ",
+    data: "2030.08.05"
+  },
+  {
+    link: "#",
+    image: ImageBlog03,
+    image2x: ImageBlog2x03,
+    imageWebp: ImageWebpBlog03,
+    imageWebp2x: ImageWebp2xBlog03,
+    title: "Sky Coffeeのオリジナル曲が完成！無料ダウンロード可能です。",
+    data: "2030.08.02"
+  },
+  {
+    link: "#",
+    image: ImageBlog04,
+    image2x: ImageBlog2x04,
+    imageWebp: ImageWebpBlog04,
+    imageWebp2x: ImageWebp2xBlog04,
+    title: "ハイキング用のコーヒーを販売中です。",
+    data: "2030.07.28"
+  },
+  {
+    link: "#",
+    image: ImageBlog05,
+    image2x: ImageBlog2x05,
+    imageWebp: ImageWebpBlog05,
+    imageWebp2x: ImageWebp2xBlog05,
+    title: "ショップの壁を塗ってみました。",
+    data: "2030.07.27"
+  },
+  {
+    link: "#",
+    image: ImageBlog06,
+    image2x: ImageBlog2x06,
+    imageWebp: ImageWebpBlog06,
+    imageWebp2x: ImageWebp2xBlog06,
+    title: "ブログをはじめました。",
+    data: "2030.07.26"
+  }
+]
